79fd58f7c21da1cd4b9ac5273ad167c44c7ea519
riteshtekchandani18 <riteshtekchandani0@gmail.com>
2023-11-20 18:34:19 +0530

    Database making

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Task (models.Model):
+    title = models.CharField(max_length=200)
+    is_completed =  models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title

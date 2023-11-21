7b653319bd23c7449be7cc6a550ab15431990e88
riteshtekchandani18 <riteshtekchandani0@gmail.com>
2023-11-21 18:35:58 +0530

    loading data into page

--- home/views.py
+++ home/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Task
 # Create your views here.
 def index(request):
-    context = {}
+    context = {
+        'task': Task.objects.all()
+    }
     return render(request,'index.html',context)
 
 def add_task(request):
-    context = {}
-    return render(request,'todo_list.html ',context)
+    if request.method =='Post':
+     Task = request.Post.get('task')
+     if len(Task)>=2:
+        Task.Objects.create(title=Task)
+redirect('home')
 
-def update_task(request):
+def update_task(request,id):
     context ={ }
     return render(request , 'todo_list.html',context)
+
+def delete_task(request,id):
+    pass   
